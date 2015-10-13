b843f06ffb099f64eb73ca2d2ab3fe1dc8d4c298
Ian Macalinao <me@ian.pw>
2015-10-13 15:23:42 -0500

    Prevent duplicate loading

--- inject.js
+++ inject.js
@@ -1,15 +1,23 @@
-console.log('Injecta: Injecting scripts...');
+if (typeof document.injectaLoaded === 'undefined') {
+  document.injectaLoaded = [];
+  console.log('Injecta: Injecting scripts...');
 
-function addScript(src) {
-  var s = document.createElement('script');
-  s.setAttribute('src', src);
-  document.body.appendChild(s);
-}
+  function addScript(src) {
+    var s = document.createElement('script');
+    s.setAttribute('src', src);
+    document.body.appendChild(s);
+  }
 
-document.addEventListener('injecta_injected', function(e) {
-  e.detail.libraries.map(function(lib) {
-    console.log('Injected ' + lib.name);
-    addScript(lib.url);
+  document.addEventListener('injecta_injected', function(e) {
+    e.detail.libraries.map(function(lib) {
+      if (!!~document.injectaLoaded.indexOf(lib.name)) {
+        console.warn('Warning: Library `' + lib.name + '` was not loaded again.');
+        return;
+      }
+      console.log('Injected ' + lib.name);
+      document.injectaLoaded.push(lib.name);
+      addScript(lib.url);
+    });
   });
-  console.log('Scripts injected!');
-});
+
+}
